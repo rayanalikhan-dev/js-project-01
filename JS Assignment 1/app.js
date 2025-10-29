104f64b022f61844e8f596a288c009d0bbfc77fd
rayanalikhan-dev <rayanalikhan0710@gmail.com>
2025-10-30 00:39:53 +0500

    uploaded

---
+++ JS Assignment 1/app.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    alert("Welcome to my website!");
+  };
+
+  function showMessage() {
+    alert("please enter a valid password");
+  }
+
+  function jsLand() {
+    alert("Welcome to JS Land\nHappy Coding!");
+  }
+
+  function multipleMessage() {
+    alert("Welcome to JS Land...");
+    alert("Happy Coding!");
+ }
